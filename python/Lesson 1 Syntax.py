88d055d49ae52c4b62ff1e4f16abf1baf1dff793
21Jgit <21jwilliams98@coopbebington.co.uk>
2025-09-12 11:59:21 +0100

    Create Lesson 1 Syntax.py

---
+++ python/Lesson 1 Syntax.py
@@ -0,0 +1,22 @@
+# Starting with this line, we learn our first bit of syntax... well not really. Anything on a line after a # will not be ran.
+"""
+Same for anything between to sets of three "
+So none of this here is read.
+This one here is for multiline comments, unlike #, which only does one line
+"""
+
+# This file when ran shouldn't encounter any errors if you run it in using python 3.8 or above, I'm no expert on the earlier versions.
+# The rest of this file will be in comments so nothing is ran, other files as I make them will have run-able code, I will go over that in the next file.
+# Now, onto the actual Syntax.
+
+"""
+In Python, we don't need ; at the end of every line, this can be helpful because it saves that poor semi-colon which has been hit more times than the w, a, s, and d keys.
+Now, imagine a box with a cat in it. Let's say the cats name is "Cat", what if we also called the box "Box"?
+Well? Now we have "Cat" in Box. What is the box was "Cat" itself?
+Well... Box = "Cat"
+Congrats, we now know variables... kinda. That example was bad... Let me explain.
+Box is now like something we can talk about all the time, we ask python "Hey... What is Box?" And Python will cay "Cat". That's a very simple version of what is happening here.
+I'll go more in depth on this in the Output lesson and Input lesson.
+"""
+
+# Note from me... This file is kinda useless exept teaching about how to do comments... So... Feel free to ignore it haha
